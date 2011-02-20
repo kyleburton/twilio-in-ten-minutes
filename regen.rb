0b3f4401ee75465d98e715021bcdede72f85c0f2
Kyle Burton <kburton@relaynetwork.com>
2011-02-20 11:44:04 -0500

    more images in the guid, first gen of rails sandbox app

--- regen.rb
+++ regen.rb
@@ -17,13 +17,22 @@ def what_changed
     end
   end
   changed
+rescue
+  puts "Someting went wrong? : #$!"
 end
 
+def regen
+  puts ".running redcloth..."
+  system "redcloth README.textile > README.textile.html"
+  puts ".regenerated."
+end
+
+regen
 while true
   sleep 1
   changed = what_changed
   if !changed.empty?
-    puts "CHANGE: #{changed.inspect}"
-    system "redcloth README.textile > README.textile.html"
+    puts "CHANGED: #{changed.inspect}"
+    regen
   end
 end
