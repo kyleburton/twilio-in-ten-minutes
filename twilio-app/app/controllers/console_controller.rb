6a2a5e34e9edeef29e12da25f5a1629df16d15bb
Kyle Burton <kburton@relaynetwork.com>
2011-03-09 21:29:43 -0500

    starting on the console

--- twilio-app/app/controllers/console_controller.rb
+++ twilio-app/app/controllers/console_controller.rb
@@ -2,4 +2,25 @@ class ConsoleController < ApplicationController
   layout 'site'
   def index
   end
+
+  def active_sessions
+    @active_sessions = [
+      { :id => 1, :name => 'active session1' },
+      { :id => 2, :name => 'active session2' },
+      { :id => 3, :name => 'active session3' },
+      { :id => 4, :name => 'active session4' },
+    ]
+    respond_to do |fmt|
+      fmt.json { render :json => @active_sessions }
+    end
+  end
+
+  def session_info
+    @session_info = { :name  => 'active session1', 
+                      :state => 'start' }
+
+    respond_to do |fmt|
+      fmt.json { render :json => @session_info }
+    end
+  end
 end
--- twilio-app/public/javascripts/console.js
+++ twilio-app/public/javascripts/console.js
@@ -1,9 +1,52 @@
 /* console.js */
 
-var TwilioApp = function () {
+var Console = function () {
   var self = {};
 
+  self.initialize = function () {
+    self.fetchActiveSessions();
+  };
 
+  self.fetchActiveSessions = function () {
+    $.ajax({
+      url: '/console/active_sessions',
+      data: { },  
+      success: self.receiveActiveSessions,
+      error: self.ajaxError,
+      dataType: 'json'
+    }); 
+    return false;
+  };
+
+  self.ajaxError = function (xhr, err) {
+    console.log('err=%s', err);
+    console.log('xhr=%s', xhr);
+  };
+
+  self.receiveActiveSessions = function (data) {
+    console.log('active sessions[len=%s]=%s', data.length, data);
+    $.each(data, function(idx,sessionInfo) {
+        console.log('data[%s] entry=%s', idx, sessionInfo);
+        console.dir(sessionInfo);
+        $('#active-sessions').append(
+          $('<p>').append(sessionInfo.id + " : ")
+                  .append( $('<a>').attr('href','#')
+                                   .attr('session-id',sessionInfo.id)
+                                   .click(self.clickSessionLink)
+                                   .append(sessionInfo.name) )
+        );
+    });
+    return true;
+  };
+
+  self.clickSessionLink = function (event) {
+    self.last = event;
+    console.log('event=%s', event);
+    var sessionId = $(event.target).attr("session-id");
+    return false;
+  }
 
   return self;
 }();
+
+$(document).ready(Console.initialize);
