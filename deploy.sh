9361d833c535dbee6106950800b9a28a864079ba
Kyle Burton <kburton@relaynetwork.com>
2011-06-01 13:54:10 -0400

    change rebase to merge

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 set -e
 git checkout twilio-deploy 
-git rebase master
-git pull --rebase heroku master
+git merge master
+git pull heroku master
 git push heroku twilio-deploy:master
 git checkout master
